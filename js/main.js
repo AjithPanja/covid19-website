40fc9a93d0538e06b1fdbb255c60f540cbdfad10
AJITH KUMAR PANJA <ajithkumarpanja@gmail.com>
2020-04-05 04:11:35 +0530

    map rectified

--- js/main.js
+++ js/main.js
@@ -18,8 +18,8 @@ function getData(){
 }
 function get_state_details(j){
 	d3.select('svg').remove()
-	var width = 950,
-    height = 500;
+	var width = 480,
+    height = 450;
 	var pat = "maps/"+j+".json";
 	var ob = j+"_district";
 	if(j=="india")
@@ -31,7 +31,9 @@ function get_state_details(j){
 	
 	var svg = d3.select("#graph").append("svg")
     .attr("width", width)
-    .attr("height", height);
+    .attr("height", height)
+	.attr("viewBox","0 0 600 500")
+	.attr("preserveAspectRatio","xMidYMid meet")
 	
 	
 	
@@ -40,17 +42,195 @@ function get_state_details(j){
 		var featureCollection = topojson.feature(topology, topology['objects'][ob]);
 		var bounds = d3.geo.bounds(featureCollection);
 		console.log(bounds);
-		var sc = 25000/(bounds[1][1]-bounds[0][1]);
-		console.log();
+		var sc = 20000/(bounds[1][1]-bounds[0][1]);
+		var ad = 0;
+		var bd = 0;
+		if(j=="india")
+		{
+			sc = 1000;
+			ad = 10;
+			bd = 1.5;
+		}
+		else if(j=="tamilnadu")
+		{
+			sc = 1200;
+			ad = 10;
+			bd = 1.5;
+		}
+		else if(j=="kerala")
+		{
+			sc = 5000;
+			ad = 3;
+			bd = 0;
+		}
+		else if(j=="andhrapradesh")
+		{
+			sc = 3500;
+			ad = 3;
+			bd = 0;
+		}
+		else if(j=="karnataka")
+		{
+			sc = 4000;
+			ad = 3;
+			bd = 0;
+		}
+		else if(j=="telangana")
+		{
+			sc = 5200;
+			ad = 2;
+			bd = 0;
+		}
+		else if(j=="maharashtra")
+		{
+			sc = 3500;
+			ad = 3;
+			bd = 0;
+		}
+		else if(j=="madhyapradesh")
+		{
+			sc = 3500;
+			ad = 3;
+			bd = 0;
+		}
+		else if(j=="odisha")
+		{
+			sc = 4500;
+			ad = 2;
+			bd = 0;
+		}
+		else if(j=="chhattisgarh")
+		{
+			sc = 4500;
+			ad = 2;
+			bd = 0;
+		}
+		else if(j=="westbengal")
+		{
+			sc = 4500;
+			ad = 3;
+			bd = 0;
+		}
+		else if(j=="gujarat")
+		{
+			sc = 4500;
+			ad = 2.5;
+			bd = 0;
+		}
+		else if(j=="rajasthan")
+		{
+			sc = 3500;
+			ad = 3;
+			bd = 0;
+		}
+		else if(j=="uttarpradesh")
+		{
+			sc = 4000;
+			ad = 2.5;
+			bd = 0;
+		}
+		else if(j=="haryana")
+		{
+			sc = 7000;
+			ad = 1.5;
+			bd = 0;
+		}
+		else if(j=="punjab")
+		{
+			sc = 7500;
+			ad = 1.5;
+			bd = 0;
+		}
+		else if(j=="himachalpradesh")
+		{
+			sc = 7500;
+			ad = 1.5;
+			bd = 0;
+		}
 		
-		var centerX = d3.sum(bounds, function(d) {return d[0];})/2,
-		centerY = d3.sum(bounds, function(d) {return d[1];})/2 ;
+		else if(j=="uttarakhand")
+		{
+			sc = 7500;
+			ad = 1.5;
+			bd = 0;
+		}
+		else if(j=="arunachalpradesh")
+		{
+			sc = 5000;
+			ad = 2;
+			bd = 0;
+		}
+		else if(j=="nagaland")
+		{
+			sc = 10000;
+			ad = 1;
+			bd = 0;
+		}
+		else if(j=="manipur")
+		{
+			sc = 10000;
+			ad = 1;
+			bd = 0;
+		}
+		else if(j=="mizoram")
+		{
+			sc = 10000;
+			ad = 1.5;
+			bd = 0;
+		}
+		else if(j=="tripura")
+		{
+			sc = 12000;
+			ad = 1;
+			bd = 0;
+		}
+		else if(j=="assam")
+		{
+			sc = 5000;
+			ad = 2;
+			bd = 0;
+		}
+		else if(j=="meghalaya")
+		{
+			sc = 7000;
+			ad = 2;
+			bd = 0;
+		}
+		else if(j=="bihar")
+		{
+			sc = 5000;
+			ad = 2;
+			bd = 0;
+		}
+		else if(j=="jharkhand")
+		{
+			sc = 6000;
+			ad = 1.5;
+			bd = 0;
+		}
+		else if(j=="ladakh")
+		{
+			sc = 3000;
+			ad = 2.5;
+			bd = 0;
+		}
+		else if(j=="sikkim")
+		{
+			sc = 15000;
+			ad = 0.75;
+			bd = 0;
+		}
+		console.log("scale :" + sc);
+		var centerX = (bounds[0][0]+bounds[1][0])/2 + ad,
+		centerY = (bounds[0][1]+bounds[1][1])/2 + bd;
 		
 		
-		
+		var touchtime = 0;
+var delay = 800;
+var action = null;
 		var projection = d3.geo.mercator()
 		.scale(sc)
-		.center([centerX, centerY]);
+		.center([centerX,centerY]);
 		
 		path.projection(projection);
 		
@@ -59,8 +239,21 @@ function get_state_details(j){
 		.enter().append("path")
 		.attr("d", path)
 		.on('click', function(d){
-			var l = d['properties']['st_nm'];
-			summa(l);
+			if((new Date().getTime() - touchtime) < delay){
+				clearTimeout(action)
+				alert('dbl');
+				touchtime=0;
+				var l = d['properties']['st_nm'];
+				summa(l);
+			}
+			/* Single Click */
+			else{
+				touchtime = new Date().getTime();
+				action = setTimeout(function(){
+					alert('single');
+				},delay);
+			}
+			
 		})
 		.on('mouseout', function(d){
 			document.getElementById('name').innerHTML= "District/State"; 
@@ -81,20 +274,20 @@ function get_state_details(j){
 				var now=0;
 				for(k=0;k<a.length;k++)
 				{
-				//console.log(a[k]['state']);
-				
-				if(a[k]['state']==name)
-				{//console.log(a[k]);
-					for(i=0;i<a[k]['districtData'].length;i++)
-					{			
-								
-								confirmed += a[k]['districtData'][i]['confirmed'];
-								now += a[k]['districtData'][i]['delta']['confirmed'];
-								
-								//console.log(confirmed);
+					//console.log(a[k]['state']);
+					
+					if(a[k]['state']==name)
+					{//console.log(a[k]);
+						for(i=0;i<a[k]['districtData'].length;i++)
+						{			
+							
+							confirmed += a[k]['districtData'][i]['confirmed'];
+							now += a[k]['districtData'][i]['delta']['confirmed'];
+							
+							//console.log(confirmed);
 						}
 					}
-					}	
+				}	
 			}
 			else
 			{
@@ -104,21 +297,21 @@ function get_state_details(j){
 				{	
 					if(a[start]['districtData'][i]['district'] == name)
 					{		
-							var confirmed = a[start]['districtData'][i]['confirmed'];
-							var now = a[start]['districtData'][i]['delta']['confirmed']
-							//console.log(confirmed);
+						var confirmed = a[start]['districtData'][i]['confirmed'];
+						var now = a[start]['districtData'][i]['delta']['confirmed']
+						//console.log(confirmed);
 					}
-					}
+				}
 			}
 			if(!confirmed)
 			{
 				confirmed=0;
 				now=0;
-				}
-			 document.getElementById('name').innerHTML= name; 
-			 document.getElementById('count').innerHTML= confirmed; 
-			 document.getElementById('now').innerHTML= now; 
-		
+			}
+			document.getElementById('name').innerHTML= name; 
+			document.getElementById('count').innerHTML= confirmed; 
+			document.getElementById('now').innerHTML= now; 
+			
 		});
 	});
 }
@@ -170,15 +363,15 @@ function draw_chart(){
 			onClick: graphClickEvent,
 			scales: {
 				xAxes: [{ stacked: false ,autoSkip: false,gridLines: {
-                display:false
-            }}],
+					display:false
+				}}],
 				yAxes: [{
 					ticks: {
 						beginAtZero:true
 					},
 					gridLines: {
-                display:false
-            },
+						display:false
+					},
 					stacked: false
 				}]
 			},
@@ -198,10 +391,10 @@ function summa(i){
 	{
 		if(a[j]['state']==i)
 		{
-		start = j;
-		break;
+			start = j;
+			break;
 		}
-		}
+	}
 	
 	
 	i = i.replace(/ /g,'');
@@ -218,7 +411,7 @@ function graphClickEvent(event, array){
 	var i = array[0]['_view']['label'];
 	start = array[0]['_index'];
 	
-	 window.location = "#get_Map";
+	window.location = "#get_Map";
 	i = i.replace(/ /g,'');
 	i = i.toLowerCase();
 	
@@ -228,3 +421,4 @@ function graphClickEvent(event, array){
 	//console.log(data[i]);
 	get_state_details(i);
 }	
+
